f4e6d24311c4b090686765657bc39bc657de6068
DrPaulWilliamson <P.Williamson@liv.ac.uk>
2024-09-19 15:37:25 +0100

    Initial version for posting to Github

--- R/round.R
+++ R/round.R
@@ -1,9 +1,21 @@
+#' Round numerical data
+#'
+#' @param data_in The data to be rounded, supplied as a (vector of) numeric or logical; or as data object of class table, matrix, data.frame or tibble (any of which can contain non-numeric columns)
+#' @param dp The number of decimal places to round to (default is 2), supplied as either as single number to be applied to all numeric values, or as a series vector of values (one per column of the input data object, in column order, using NA for factor/string columns)
+#' @param as_string If TRUE (default is FALSE), and data_in is of class table, returns a table in which all numeric values have been converted to strings, so that the dp per column used for display matches that used for rounding
+#'
+#' @return An updated version of the supplied data object containing rounded numeric values
+#' @export
+#'
+#' @examples
+#' new_round( data_in = 1.1234 )
+#' new_round( data_in = rep(1.1234, 4), dp = c(1,2,3,4) )
+#' df <- data.frame( var = c("A", "B", "C"), v1 = rep(1.1234, 3), v2 = rep(1.1234, 3) )
+#' new_round( df, dp = c(2, 1) )
+#' # Example of table with as_string = FALSE to follow
+#' new_round( tibble::as_tibble( df), dp = c(NA, 2, 1) )
+#'
 new_round <- function( data_in, dp = 2, as_string = TRUE ) {
-
-  ## as_string - forces results to be reported as strings for tables
-  ##             so that displayed dp per column match rounded dp per column
-  ##             option ignored for all other data types since numeric output
-  ##             displays correct dp per column
 
   data_in_class <- class(data_in)
 
@@ -49,12 +61,33 @@ new_round <- function( data_in, dp = 2, as_string = TRUE ) {
         call. = F )
   }
 
+  # For other valid data types, find number of columns in dataset,
+  # check that dp_length matches data_length, and take corrective
+  # action if required
+
+  if ( is.table(data_in) | is.matrix(data_in) |
+       is.data.frame(data_in) | tibble::is_tibble(data_in) ) {
+
+    data_length <- ncol( data_in )
+
+    if ( dp_length !=1 & data_length != dp_length ) {
+      warning( "Number of dp values supplied does not match number",
+             " of columns\nin data being rounded. Therefore results ",
+             "might not be as intended.", call. = F )
+
+      # if dp has too few values, extend as required with default value of 2
+      # and update dp_length accordingly
+      if ( dp_length < data_length ) {
+        dp <- c( dp, rep(2, data_length - dp_length) )
+        dp_length <- data_length
+      }
+    }
+  }
+
   # tables and matrices
   if ( is.table(data_in) | is.matrix(data_in) ) {
 
     if ( is.numeric(data_in) ) {
-
-      data_length <- ncol( data_in )
 
       if ( dp_length == 1) {
 
@@ -81,11 +114,6 @@ new_round <- function( data_in, dp = 2, as_string = TRUE ) {
                                        nsmall = dp[i] )
           }
         } # as_string = FALSE/TRUE
-
-        if ( data_length != dp_length )
-          warning( "Number of dp values supplied does not match number",
-                   " of columns\nin in data being rounded. Therefore results ",
-                   "might not be as intended.", call. = F )
 
       } # dp_length = 1 or 1+
 
@@ -150,7 +178,7 @@ new_round <- function( data_in, dp = 2, as_string = TRUE ) {
         }
       }
     }
-  } # is.tibble
+  } # is_tibble
 
 
   # If data_length undefined at this stage, input data is not one of
